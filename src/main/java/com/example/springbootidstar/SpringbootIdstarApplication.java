252e2730b9f7a12b2d558ad4368269917bfd64b8
Akhmad Bilyyasif <ids.akhmadb@xl.co.id>
2023-04-10 10:45:49 +0700

    git

---
+++ src/main/java/com/example/springbootidstar/SpringbootIdstarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootidstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIdstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIdstarApplication.class, args);
+	}
+
+}
